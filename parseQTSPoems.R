53dd24b2c85acbcfaa7e12e3f4f5a5e6b3bba823
hyginn <boris.steipe@utoronto.ca>
2016-05-09 00:01:02 -0400

    V 1.0 - builds database

--- parseQTSPoems.R
+++ parseQTSPoems.R
@@ -25,14 +25,21 @@
 # GB18030 (which supersedes GB2312 and supports both simplified and traditional
 # characters). For encoding details see https://en.wikipedia.org/wiki/GB_18030
 # 
-# The following poem files were manually repaired.
-#    qts_0540.htm was missing
+# For file repair documentation: see notes in repairShitanSources.R
 # 
 #
-# V 0.1
+# V 1.0
 # Date:     May. 2016
 # Author:   Boris Steipe and Yi Chen
 #
+# ToDo      Move flagsDF and poemFlagsDF in poemAnalysis.R
+#           - Flag title ellipsis
+#           - unknown characters
+#           Refine parsing
+#           Improve documentation
+#           
+# V 1.0     - use Unicode replace glyph "�" for unknown characters.
+#           - most requirements implemented
 # V 0.1     write functions to extract and analyse poems
 #
 # ==============================================================================
@@ -59,6 +66,40 @@ if (!require(Unicode)) {
   library(Unicode)
 }
 
+
+# ==== DEFINITIONS =============================================================
+
+notesDF <- data.frame(notesID = numeric(),
+                      name = character(),
+                      definition = character(),
+                      stringsAsFactors = FALSE)
+
+i<- 1
+notesDF$notesID[i] <- i 
+notesDF$name[i] <- "TITLETRUNC" 
+notesDF$definition[i] <- "Title truncated, contains ellipsis \"…\"." 
+
+i<- 2
+notesDF$notesID[i] <- i 
+notesDF$name[i] <- "UNKCHAR" 
+notesDF$definition[i] <- "Unknown or untranscribable character(s) replaced with \"�.\"" 
+
+i<- 3
+notesDF$notesID[i] <- i 
+notesDF$name[i] <- "MISSCHAR" 
+notesDF$definition[i] <- "Character missing in QTS source shown with \"□\"." 
+
+i<- 4
+# this flag is used for poems with "X首" notes in the title, as well as
+# poems that contain "¤" as a stanza annotation
+notesDF$notesID[i] <- i 
+notesDF$name[i] <- "TODOMULTISECT" 
+notesDF$definition[i] <- "Poem has multiple sections or stanzas that need to be annotated." 
+
+i<- 5
+notesDF$notesID[i] <- i 
+notesDF$name[i] <- "MULTISECT" 
+notesDF$definition[i] <- "Poem has multiple sections or stanzas that have been annotated." 
 
 
 # ==== FUNCTIONS ===============================================================
@@ -88,11 +129,22 @@ extractPoems <- function(fileName) {
   # remove homepage link at end
   txt <- gsub("<a href=\"index.htm\">回目录</a>", "", txt)
   
-  # replace links to character image with "?"
-  txt <- gsub("<a href=\"\\.\\./BZK/\\w+\\.jpg\"target=\"_blank\">[^<]+</a>", "?", txt)
+  # replace links to character image with "�"
+  txt <- gsub("<a href=\"\\.\\./BZK/\\w+\\.jpg\"target=\"_blank\">za</a>zb", "��", txt)
+  txt <- gsub("<a href=\"\\.\\./BZK/\\w+\\.jpg\"target=\"_blank\">[^<]+</a>", "�", txt)
+  
+  # handle shigeku AND shitan style titles
+  txt <- gsub("「", "【", txt)
+  txt <- gsub("」", "】", txt)
+  
+  # normalize "" to "�": - QTS shows these to be untranscribable, not missing
+  txt <- gsub("", "�", txt)
+
+  # normalize "xX々" to "��"
+  txt <- gsub("[a-zA-Z0-9]{2}々", "��", txt)
   
   # test titles
-  if (gregexpr("【", txt)[[1]][1] == -1) {
+  if (gregexpr("[【]", txt)[[1]][1] == -1) {  
     stop(sprintf("Volume in file %s: no match for \"【\".", fileName))
   }
   nTitles <- length(gregexpr("【", txt)[[1]])
@@ -104,14 +156,16 @@ extractPoems <- function(fileName) {
   nAuthors <- length(gregexpr("<font face=\"黑体\">", txt)[[1]])
 
   if (nTitles != nAuthors) {
-    stop(sprintf("Volume in file %s: mismatch between number of titles and authors.", fileName))
+    print(sprintf("Warning: Volume in file %s: 
+                   mismatch between number of titles (%d) and authors(%d) .",
+                  fileName, nTitles, nAuthors))
   }
   
   # add period after poem author
   txt <- gsub("<font face=\"黑体\">([^<]*)</font>", "author\\1。", txt)
   
   # replace with "unknown" if missing
-  txt <- gsub("author。", "未名。", txt)
+  txt <- gsub("author。", "未知。", txt)
   txt <- gsub("author", "", txt)
   
   # add period after "drinking game" author
@@ -130,17 +184,20 @@ extractPoems <- function(fileName) {
                                   # 
   
   # replace punctuation and line-breaks with a single blank
-  txt <- gsub("[。， \\s\r\n]+", " ", txt)
+  txt <- gsub("[。，、：！？； \\s\r\n]+", " ", txt)
+  
+  # replace multiple blanks with a single blank
+  txt <- gsub("\\s{2,}", " ", txt)
   
   # replace fullwidth parentheses with plain parentheses
   txt <- gsub("（", "(", txt)
   txt <- gsub("）", ")", txt)
   
-  # delete all comments (in parentheses)
-  txt <- gsub("\\([^)]*\\)", "", txt)
+  # do not delete comments (in parentheses)
+  # txt <- gsub("\\([^)]*\\)", "", txt)
   
-  # replace U+25A1 WHITE SQUARE □ with questionmark
-  txt <- gsub("□", "?", txt)
+  # don't replace U+25A1 WHITE SQUARE □ with questionmark
+  # txt <- gsub("□", "?", txt)
 
   # reattach "drinking game" author to preceding verse
   txt <- gsub(" --", "--", txt)
@@ -169,10 +226,12 @@ extractPoems <- function(fileName) {
 analysePoem <- function(p, i) {
   # analyse a QTS poem.
   # parameters: p  a QTS poem as extracted by extractPoems()
+  #             i  an index associated with the poem 
   # Value: a list object with QTS number, authorID, title, and body
 
   poem <- list()
   poem$body <- p
+  poem$flags <- character(0)
   
   # extract QTS number
   regEx <- "^(\\d+)"
@@ -199,32 +258,44 @@ analysePoem <- function(p, i) {
   # extract Author
   regEx <- "^([^ ]+) "
   m <- regexec(regEx, poem$body)
-  if (m[[1]][1] == -1) {stop(sprintf("Poem %d: no author found.", i))}
+  if (m[[1]][1] == -1) {print(sprintf("Warning: Poem %d: no author found.", i))}
   aut <- regmatches(poem$body, m)[[1]][2]
   poem$authorID <- authorDF$authorID[authorDF$nameS == aut]
-  if (length(poem$authorID) != 1) {
-    print(sprintf("Warning: Poem %d: author %s not in DB.", i, aut))
-    poem$authorID <- authorDF$authorID[authorDF$nameS == "未名"]
+  if (length(poem$authorID) > 1) {
+    stop(sprintf("Stop: Poem %d: author %s is not unique in DB.", i, aut))
+  } else if (length(poem$authorID) == 0) {
+    poem$authorID <- aut  # to be added to authorDF
   }
   poem$body <- gsub(regEx, "", poem$body)
+  
+  # annotate 
+  poem$flags <- numeric(0)
 
+  if (length(grep("…", poem$title)) != 0) { poem$flags <- c(poem$flags, 1)} # TITLETRUNC
+  if (length(grep("�", poem$body))  != 0) { poem$flags <- c(poem$flags, 2)}  # UNKCHAR
+  if (length(grep("□", poem$body)) != 0) { poem$flags <- c(poem$flags, 3)}  # MISSCHAR
+  if (length(grep("[一二三四五六七八九十]首", poem$title)) != 0 |          # TODOMULTISECT
+      length(grep("¤", poem$body) != 0)) { 
+    poem$flags <- c(poem$flags, 4)
+  }  
+  
   return(poem)
 }
 
 
-validateCJK <- function(s, skipUnk = TRUE) {
-  # validate that the Characters in "s" are all in the CJK unified Block
-  # some exceptions are passed in vector "allowed"
+validateCJK <- function(s) {
+  # Validate that the Characters in "s" are all in the CJK unified Block.
+  # Allowed exceptions are defined in vector "allowed".
 
-  if (skipUnk) {
-    allowed <- c(" ", "(", ")", "-", "?")
-  } else {
-    allowed <- c(" ", "(", ")", "-")
-  }
+  allowed <- c(" ", "-", "(", ")", "“", "”", "‘", "’", "《", "》", "…", "□", "·", "¤", "�")
   
+  CJKBlocks <- c("CJK Unified Ideographs",
+                 "CJK Symbols and Punctuation",
+                 "CJK Unified Ideographs Extension A")
+
   props <- u_char_property(utf8ToInt(s), which = "Block")
   chars <- unlist(strsplit(s, ""))
-  x <- which(props != "CJK Unified Ideographs")
+  x <- which(! props %in% CJKBlocks)
   if (length(x) == 0 | all(chars[x] %in% allowed)) {
     # all validated
     return(0)
@@ -251,7 +322,7 @@ nPoems <- 45000 # estimate - see above
 poemDF <- data.frame(poemID   = numeric(nPoems),
                      QTSvol   = numeric(nPoems),
                      QTSnum   = numeric(nPoems),
-                     period   = character(nPoems),
+                     ShitanNum  = numeric(nPoems),
                      authorID = numeric(nPoems),
                      titleS   = character(nPoems),
                      bodyS    = character(nPoems),
@@ -261,24 +332,40 @@ for (i in 1:nFiles) {
   print(sprintf("Volume: %d", i))
   po <- extractPoems(sprintf("%s/%s", poemDir, poemDocs[i]))
   for (j in 1:length(po)) {
-#    poemID <- max(poemDF$poemID) + 1
-#    rowNum <- min(which(poemDF$poemID == 0))
+    poemID <- max(poemDF$poemID) + 1
+    rowNum <- min(which(poemDF$poemID == 0))
     
     poem <- analysePoem(po[j], j)
     
-    s <- sprintf("%d.%d \"%s\" (%s) %s\n",
-                i, poem$qtsNum, poem$title,
-                authorDF$nameS[authorDF$authorID == poem$authorID],
-                substr(poem$body, 1, 20))
+    # s <- sprintf("%d.%d \"%s\" (%s) %s\n",
+    #             i, poem$qtsNum, poem$title,
+    #             authorDF$nameS[authorDF$authorID == poem$authorID],
+    #             substr(poem$body, 1, 20))
     if (validateCJK(poem$title)[1] != 0) {
       print(paste("Warning: Non CJK in title: ", validateCJK(poem$title), s))
     }
     if (validateCJK(poem$body)[1] != 0) {
-      print(paste("Warning: Non CJK in body: ", validateCJK(poem$body), s))
+      print(paste("Warning: Non CJK in body of No.", j, ": ", validateCJK(poem$body), s))
     }
-#    if (readline(prompt = "next poem> ") == "x") { break() }
+    
+    if (is.character(poem$authorID)) {
+      print(sprintf("Warning: Poem %d: author not in DB. Adding author %s.", j, poem$authorID))
+      iRow <- nrow(authorDF) + 1
+      authorDF[iRow, "authorID"] <- max(authorDF$authorID) + 1
+      authorDF[iRow, "QTSbio"]   <- "未知。"
+      authorDF[iRow, "nameS"]    <- poem$authorID
+    }
+    
+    # add this poem to DF:
+    poemDF[rowNum, "poemID"] <- poemID
+    poemDF[rowNum, "QTSvol"] <- i
+    poemDF[rowNum, "QTSnum"] <- j
+    poemDF[rowNum, "ShitanNum"] <- poem$qtsNum  # confusing, but correct
+    poemDF[rowNum, "authorID"] <- poem$authorID
+    poemDF[rowNum, "titleS"] <- poem$title
+    poemDF[rowNum, "bodyS"] <- poem$body
   }
-  if (readline(prompt = "next volume> ") == "x") { break() }
+  # if (readline(prompt = "next volume> ") == "x") { break() }
 }
 
 # save(...)
@@ -290,7 +377,7 @@ for (i in 1:nFiles) {
 
 utf8ToInt("（")
 u_char_inspect(utf8ToInt("帝京篇十首"))
-u_char_inspect(utf8ToInt("（"))
+u_char_inspect(utf8ToInt("‘‘"))
 u_char_inspect(utf8ToInt("-"))
 
 #    
