3f6bf21d5496036db274d148716b77a96185d3f7
hyginn <boris.steipe@utoronto.ca>
2016-06-14 07:46:28 -0400

    Practice code

--- test.R
+++ test.R
@@ -1,47 +1,14 @@
 
 
-linkURL <- "http://www.shigeku.org/shiku/gs/tangshi/"
+test <- c("this", "that", "", "some more")
+test
+test != ""
+x <- test != ""
+test[x]
 
+test[test != ""]
 
-x <- readLines(linkURL)
-head(x, 20)
+test <- test[test != ""]
 
-# this is encoded with the gb2312 charcter encoding. inconv() will
-# convert to utf-8
+test
 
-y <- iconv(x, "GB2312", "UTF8")
-head(y, 20)
-tail(y, 20)
-
-
-# ToDo:
-#
-# Parse all lines and make a list of URLs to process
-# 
-
-m <- regexpr("(qts_\\d{4}.htm)", y)
-urlList <- c(urlList, regmatches(y, m))
-  
-
-# 
-# 
-# 
-# 
-# For each URL in list:
-
-for (i in 1:length(urlList)) {
-  if nchar(urlList[i]) > 1 {
-    url <- paste(linkURL, urlList[i], sep="")
-    print(paste("downloading", urlList[i]))
-    x <- readLines(url)
-    y <- iconv(x, "GB2312", "UTF8")
-    fileName <- paste("../data/", urlList[i], sep="")
-    writeLines(y, fileName)
-  }
-}
-
-
-#    fetch the link
-#    Convert to utf-8
-#    Make a filename
-#    save as a file in the data-directory.
