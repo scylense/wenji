72b1a32a6af0e31c34ad4358e8f619a6aa2d88e1
hyginn <boris.steipe@utoronto.ca>
2016-06-29 09:35:36 -0400

    Add stemming, begin code for lemmatization dictionary

--- analyseAtemwende.R
+++ analyseAtemwende.R
@@ -9,12 +9,18 @@
 #                
 ## 
 #
-# V 0.5.2
+# V 0.6
 # Date:     June 2016
 # Author:   Boris Steipe and Yi Chen
 #
-# ToDo      ... 
-#           
+# ToDo:     - Refactor!
+#           - Ensure lemmata are also tokens in lemma dictionary
+#           - Complete code for lemmatization dictionary
+#           - Handle case
+#           - Review WP corpus (some residual markup still present)
+#
+# V 0.6     - add stemming for frequency analysis,
+#           - create lemmatization dictionary (incomplete)           
 # V 0.5.1   print enrichment analysis into nice table
 # V 0.5     add enrichment analysis
 # V 0.4     add frequency analysis from samples with equal numbers of words
@@ -49,14 +55,137 @@ if (!require(Unicode)) {
   library(Unicode)
 }
 
+# SnowballC for stemming
+if (!require(SnowballC)) {
+  install.packages("SnowballC")
+  library(SnowballC)
+}
+
+# tm for managing a corpus
+if (!require(tm)) {
+  install.packages("tm")
+  library(tm)
+}
+
 
 # ==== DEFINITIONS =============================================================
+
 
 # ...
 
 # ==== FUNCTIONS ===============================================================
 
+inFile <- "../data/morphy/morphy-mapping-20110717.csv"
 
+
+makeLemmaDict <- function(fileName) {
+  # Creates a lemma dictionary from data found in inFile. inFile 
+  # must be a tab separated text document that contains (optionally) comment 
+  # lines prefixed with a "#", no header, and data lines in which the first column is a
+  # full-form and the second column contains lemmata. The full form is treated as case
+  # sensitive. In cases where the full form is ambiguous, we replace it with a
+  # mixed lemma: eg:
+  #    abduzierte   abduzieren
+  #    abduzierte   abduziert
+  # is replaced with the unique entry
+  #    abduzierte   abduzier(t/en)
+  # Since all full-forms are then unique, we can use them as rownames for
+  # efficient lookup of lemmata.
+  # 
+  tmp <- read.csv(fileName,
+                  comment.char = "#",
+                  blank.lines.skip = TRUE,
+                  header = FALSE,
+                  sep = "\t",
+                  stringsAsFactors = FALSE)
+  
+  # The Morphy dictionary seems to contain erroneous mappings, in which the
+  # token is upper-case but the lemma is lower case. Perhaps these are
+  # sentence-initial forms? Whatever the reason, if the lemma is lowercase then
+  # we convert the token to lowercase.
+  ucTokens <- grep("^[A-ZÄÖÜ]", tmp[ , 1])  # find all noun-case tokens
+  ucLcMixed <- ucTokens[grepl("^[a-zäöü]", tmp[ucTokens, 2])] # which of these
+                                                              # have lowercase
+                                                              # lemmas?
+  tmp[ucLcMixed, 1] <- tolower(tmp[ucLcMixed, 1])   # make these mixed entries
+                                                    # lowercase
+  
+  # find index of all duplicated tokens
+  dupIdx <- which(duplicated(tmp[ , 1]))
+  
+  # make a copy of tmp containing only unique tokens
+  tmpU <- tmp[- dupIdx, ]
+  
+  # make a list of unique duplicates (i.e. triplicates and higher are collapsed)
+  dupTokens <- unique(tmp[dupIdx, 1])
+  
+  for (token in dupTokens) {
+    # Note: this loop takes about 20 minutes on the original Morphy dictionary
+    # retrieve all lemmata for this token
+    lemmata <- tmp[grep(token, tmp[ , 1]), 2]
+    
+    # replace the lemma in the unique-tokens-dataframe with a poly lemma
+    # constructed from the lemmata
+    tmpU[tmpU[ , 1] == token, 2] <- makePolyLemma(lemmata)
+  }
+
+  # finally make a new, single column dataframe in which rownames are tokens
+  # and the entries are lemmata
+  myDict  <- data.frame(tmpU[ , 2], stringsAsFactors = FALSE)
+  rownames(myDict)  <- tmpU[ , 1]
+  names(myDict) <- "Lemma"
+  
+  return(myDict)
+}
+
+makePolyLemma <- function(x) {
+  # Convert tokens in x into a poly lemma.
+  # 
+
+  if (length(x) == 0) { return("") }
+
+  x <- unique(x) # collapse duplicated lemmata 
+  if (length(x) == 1) { return(x[1]) }
+  
+  l <- max(nchar(x))  # length of longest word
+  # put all words into a character matrix and pad to l with "" 
+  charMat <- character()
+  for (i in 1:length(x)) {
+    charMat <- rbind(charMat, c(unlist(strsplit(x[i], "")),
+                                character(l - nchar(x[i]))))
+  }
+  
+  # find the longest common prefix
+  prefix <- character()
+  for (i in 1:l) {
+    if (length(unique(charMat[ , i])) > 1) { 
+      break
+    }
+  }
+  if (i > 1) {
+    prefix <- paste(charMat[1, 1:(i-1)], collapse = "")
+  } else {
+    prefix <- ""
+  }
+  
+  # collect all specific suffixes using index i from above
+  suffixes <- character()
+  for (j in 1:length(x)) {
+    suffixes[j] <- paste(charMat[j, i:l], collapse = "")
+  }
+  # order by length, shortest first
+  suffixes <- suffixes[order(nchar(suffixes), decreasing = FALSE)]
+
+  # assemble the polylemma, collapsing suffixes into a "/" separated string
+  lemma <- sprintf("%s(%s)", prefix, paste(suffixes, collapse = "/"))
+  
+  return(lemma)
+}
+
+myLemmaDict <- makeLemmaDict(inFile)
+save(myLemmaDict, file = "../data/myLemmaDict.1.0.RData")
+load(file = "../data/myLemmaDict.1.0.RData")
+myLemmaDict["spielte", 1]
 #
 
 
@@ -100,7 +229,7 @@ for (i in 1:length(endHyphens)) {
 }
 
 
-# make everything lower-case
+# Case-handling logic to go here, for now, we lowercase everything
 AW <- tolower(AW)
 
 # Strsplit this into words
@@ -109,20 +238,26 @@ AWwords <- unlist(strsplit(AW, "\\W+"))
 # Remove all empty words
 AWwords <- AWwords[AWwords != ""]
 
-
 # how many words are there?
 length(AWwords)  # 3556
 
+# make a copy with stemmed words
+AWstemmedWords <- wordStem(AWwords, language="de")
+
+
 # tabulate word frequencies and sort
 AWfreq <- sort(table(AWwords), decreasing = TRUE)
+AWstemmedFreq <- sort(table(AWstemmedWords), decreasing = TRUE)
 
 # how many unique words?
 length(AWfreq)  # 1765
+length(AWstemmedFreq)  # 1765
 
 
 # calculate TTR (Type Token Ratio)
-cat(sprintf("\n\n\n === TTR for Atemwende is %7.5f ===\n\n\n\n", 
-            length(AWfreq) / length(AWwords)))
+cat(sprintf("\n\n\n === TTR for Atemwende is %7.5f (stemmed:  %7.5f) ===\n\n\n\n", 
+            length(AWfreq) / length(AWwords),
+            length(AWstemmedFreq) / length(AWwords)))
 
 
 
@@ -133,7 +268,10 @@ head(AWfreq, 100)
 tail(AWfreq, 100)
 
 # plot log rank vs. log frequency (Zipf plot)
-plot(log(1:length(AWfreq)), log(as.numeric(AWfreq)))
+plot(log(1:length(AWfreq)), log(as.numeric(AWfreq)), col="seagreen")
+# compare stemmed frequencies
+points(log(1:length(AWstemmedFreq)), log(as.numeric(AWstemmedFreq)), cex = 0.7, col="firebrick")
+
 
 # look at all words that appear exactly twice
 AWfreq[AWfreq == 2]
@@ -209,18 +347,40 @@ chunks <- tolower(chunks)
 WPwords <- unlist(strsplit(chunks, "\\W+"))
 WPwords <- WPwords[nchar(WPwords) > 1]  # remove blanks and single characters
 length(WPwords)
+
+# create a stemmed version
+WPstemmedWords <- wordStem(WPwords, language = "de")
+
 WPfreq <- sort(table(WPwords), decreasing = TRUE)
 length(WPfreq)
+
+WPstemmedFreq <- sort(table(WPstemmedWords), decreasing = TRUE)
+length(WPstemmedFreq)
 
 # Calculate the TTR for the WP corpus
 cat(sprintf("\n\n\n === TTR for WP articles is %7.5f ===\n\n\n\n", 
             length(WPfreq) / length(WPwords)))
 
+cat(sprintf("\n\n\n === TTR for WP articles is %7.5f (stemmed:  %7.5f) ===\n\n\n\n", 
+            length(WPfreq) / length(WPwords),
+            length(WPstemmedFreq) / length(WPwords)))
+
+
+
 # Show a Zipf plot
 plot(log(1:length(WPfreq)), log(as.numeric(WPfreq)), col = "skyblue")
-# compare Celan
-points(log(1:length(AWfreq)), log(as.numeric(AWfreq)), col = "firebrick")
+# compare stemmed version
+points(log(1:length(WPstemmedFreq)), log(as.numeric(WPstemmedFreq)),
+       cex = 0.7,
+       col = "firebrick")
 
+# compare Celan
+points(log(1:length(AWfreq)), log(as.numeric(AWfreq)), col = "seagreen")
+
+
+# ==============================================================================
+# ANALYSIS VERSION 1: AS-IS
+# ==============================================================================
 
 # ==== Frenquency analysis with corpora of matched size
 # 
@@ -350,6 +510,137 @@ printEnrichmentTable(100, AWwords, WPwords)
 
 printEnrichmentTable(100, WPwords, AWwords)
 
+# ==============================================================================
+# ANALYSIS VERSION 2: STEMMED WORDS
+# ==============================================================================
+
+# ==== Frenquency analysis with corpora of matched size
+# 
+
+# A: make a random sample from the WP corpus
+# 
+
+corpusSize <- length(AWstemmedWords)
+RSwords <- sample(WPstemmedWords, corpusSize)
+
+# analyze
+RSfreq <- sort(table(RSwords), decreasing = TRUE)
+
+# plot
+plot(log(1:length(RSfreq)), 
+     log(as.numeric(RSfreq)),
+     xlab = "log(Rank)",
+     ylab = "log(Frequency)",
+     xlim = c(0, 8),
+     ylim = c(0, 5.2),
+     col = "seagreen")
+points(log(1:length(AWstemmedFreq)), log(as.numeric(AWstemmedFreq)), col = "firebrick")
+
+# repeat this many times and do a density plot of the sampled numbers
+# 
+
+N <- 20
+RLogRanks <- numeric()
+RLogFreq  <- numeric()
+
+for (i in 1:N){
+  RSwords <- sample(WPstemmedWords, corpusSize)
+  RSfreq  <- sort(table(RSwords), decreasing = TRUE)
+  RLogRanks <- c(RLogRanks, log(1:length(RSfreq)))
+  RLogFreq  <- c(RLogFreq,  log(as.numeric(RSfreq)))
+}
+
+# plot this as a density plot
+plot(RLogRanks, 
+     RLogFreq,
+     xlab = "log(Rank)",
+     ylab = "log(Frequency)",
+     xlim = c(0, 8),
+     ylim = c(0, 5.2),
+     col=densCols(RLogRanks,RLogFreq),
+     pch=19,
+     cex=1.5)
+points(log(1:length(AWstemmedFreq)),
+       log(as.numeric(AWstemmedFreq)),
+       pch = 19,
+       col = "firebrick",
+       cex = 0.5)
+
+
+# ==== Relative frequencies
+# 
+
+# extract the first N words from AW corpus
+N <- 100
+AWrelFreq <- AWstemmedFreq[1:N] / length(AWstemmedWords)
+
+# compile their matching positions in the WP frequency table
+# and compute the relative frequencies of these words in the WP corpus
+WPmatches <- numeric()
+WPrelFreq <- numeric()
+for (i in 1:N) {
+  M <- which(names(WPstemmedFreq) == names(AWstemmedFreq)[i])
+  if (length(M) == 1) {
+    WPmatches[i] <- M
+    WPrelFreq[i] <- WPstemmedFreq[WPmatches[i]] / length(WPstemmedwords)
+  } else {
+    WPmatches[i] <- 0
+    WPrelFreq[i] <- 0
+  }
+}
+
+# check this:
+# AWfreq[7]
+# WPfreq[WPmatches[7]]
+
+
+# print the tabulated results
+printEnrichmentTable <- function(N, X, Y) {
+  # N - number of words
+  # X, Y - words list of two corpora
+  N <- 100
+  Xfreq <- sort(table(X), decreasing = TRUE)
+  Yfreq <- sort(table(Y), decreasing = TRUE)
+  XrelFreq <- Xfreq[1:N] / length(X)
+  
+  # compile their matching positions in the WP frequency table
+  # and compute the relative frequencies of these words in the WP corpus
+  Ymatches <- numeric()
+  YrelFreq <- numeric()
+  for (i in 1:N) {
+    M <- which(names(Yfreq) == names(Xfreq)[i])
+    if (length(M) == 1) {
+      Ymatches[i] <- M
+      YrelFreq[i] <- Yfreq[Ymatches[i]] / length(Y)
+    } else {
+      Ymatches[i] <- 0
+      YrelFreq[i] <- 0
+    }
+  }
+  
+  cat("\n\n")
+  cat(" rank |   rank |              |        |        |          \n")
+  cat("    X |      Y |         word |     fX |    fY  | log-ratio\n")
+  cat("------|--------|--------------|--------|--------|----------\n")
+  logR <- log10(XrelFreq / YrelFreq)
+  for (i in order(abs(logR), decreasing = TRUE)) {
+    x <- nchar(names(Xfreq)[i])
+    cat(sprintf("  %3d | %6d | %s%s | %5.4f | %5.4f | %7.4f\n",
+                i,
+                Ymatches[i],
+                sprintf("%*s", 12 - x, " "),
+                names(Xfreq)[i],
+                XrelFreq[i],
+                YrelFreq[i],
+                logR[i]
+    ))
+  }
+}
+
+printEnrichmentTable(100, AWstemmedWords, WPstemmedWords)
+
+
+printEnrichmentTable(100, WPstemmedWords, AWstemmedWords)
 
 
 #    
