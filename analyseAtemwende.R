0bda9a86522c2d72b2f7ff2b34a0f6c7f64cf051
hyginn <boris.steipe@utoronto.ca>
2016-10-28 15:06:47 -0400

    Update to xml2, calculate frequencies etc.

--- analyseAtemwende.R
+++ analyseAtemwende.R
@@ -9,8 +9,8 @@
 #                
 ## 
 #
-# V 0.6.1
-# Date:     June 2016
+# V 0.7
+# Date:     August 2016
 # Author:   Boris Steipe and Yi Chen
 #
 # ToDo:     - Refactor!
@@ -20,6 +20,14 @@
 #           - Review WP corpus (some residual markup still present)
 #           - Do frequency analysis on lemmata.
 #           - Clean up plot labels, add legends
+#
+# History:
+# V 0.7     Use data from Mannheimer Korpus for analysis of
+#           - word frequencies
+#           - word length distributions
+#           - sentence length distributions
+#           
+#           - change code from XML package to xml2
 #
 # V 0.6.1   finish construction of lemmatization dictionary:
 #             - remove overly agressively lemmatized pronouns
@@ -44,10 +52,18 @@ getwd()
 # ==== PACKAGES ================================================================
 
 # XML parses HTML files
-if (!require(XML)) {
-  install.packages("XML")
-  library(XML)
+# if (!require(XML)) {
+#   install.packages("XML")
+#   library(XML)
+# }
+
+# xml2 replaces XML
+if (!require(xml2)) {
+  install.packages("xml2")
+  library(xml2)
 }
+
+
 
 # string handling functions, e.g. str_trim
 if (!require(stringr)) {
@@ -77,12 +93,130 @@ if (!require(tm)) {
 # ==== INITIALIZATIONS =========================================================
 
 # lod object lemmaDict
-load(file = "../data/myLemmaDict.1.1.RData")
+lemmaFile    <- "../data/lemmaDict.1.0.RData"
+load(file = lemmaFile)
 
 # ==== FUNCTIONS ===============================================================
 
+collapseHyphenation <- function(txt) {
+  # Collapse hyphenation in txt.
+  # All occurrences of -\n are deleted.
+  return(gsub("-\n", "", txt))
+}
+
+expandContractions <- function(txt) {
+  # Heuristic expansion of common German contractions and abbreviations
+  # 
+  # Note: this must run before punctuation is removed
+  
+  # The commented-out forms are considered only technically a contraction
+  # txt <- gsub("\\bim\\b", "in dem", txt)  
+  # txt <- gsub("\\bam\\b", "an dem", txt)  
+  # txt <- gsub("\\bzur\\b", "zu der", txt) 
+  # txt <- gsub("\\bzum\\b", "zu dem", txt) 
+  txt <- gsub("\\bins\\b", "in das", txt)
+  txt <- gsub("\\bans\\b", "an das", txt)
+  txt <- gsub("\\bums\\b", "um das", txt)
+  txt <- gsub("\\baufs\\b", "auf das", txt)
+  txt <- gsub("\\bhinterm\\b", "hinter dem", txt)
+  txt <- gsub("\\büberm\\b", "über dem", txt)
+  txt <- gsub("\\bunterm\\b", "unter dem", txt)
+  txt <- gsub("\\bübers\\b", "über das", txt)
+  txt <- gsub("\\bunters\\b", "unter das", txt)
+  txt <- gsub("\\bdurchs\\b", "durch das", txt)
+  txt <- gsub("\\bwenns\\b", "wenn es", txt)
+  txt <- gsub("\\bhats\\b", "hat es", txt)
+  txt <- gsub("\\bkommts\\b", "kommt es", txt)
+  txt <- gsub("\\brollts\\b", "rollt es", txt)
+  txt <- gsub("\\bichs\\b", "ich es", txt)
+  txt <- gsub("\\bins\\b", "bin es", txt)
+  
+  txt <- gsub("\\bAbb.\\b", "Abbildung", txt)
+  txt <- gsub("\\bDr.\\b", "Doktor", txt)
+  txt <- gsub("\\bgeb.\\b", "geboren", txt)
+  txt <- gsub("\\bberufl.\\b", "beruflich", txt)
+  txt <- gsub("\\babds.\\b", "abends", txt)
+  txt <- gsub("\\bmind.\\b", "mindest", txt)
+  txt <- gsub("\\btechn.\\b", "technisch", txt)
+  txt <- gsub("\\bNr.\\b", "Nummer", txt)
+  txt <- gsub("\\bMin.\\b", "Minute", txt)
+  txt <- gsub("\\bSek.\\b", "Sekunde", txt)
+  txt <- gsub("\\bMr.\\b", "Mister", txt)
+  txt <- gsub("\\bSt.\\b", "Sankt", txt)
+  txt <- gsub("\\bKrs.\\b", "Kreis", txt)
+  txt <- gsub("\\busw.\\b", "und so weiter", txt)
+  txt <- gsub("\\bWestf.\\b", "Westfalen", txt)
+  
+  
+  txt <- gsub("\\bkm\\b", "Kilometer", txt)
+  txt <- gsub("\\bkg\\b", "Kilogramm", txt)
+  
+  return(txt)
+}
 
 
+removePunctuation <- function(txt) {
+  # Replace all Punctuation characters with a blank.
+  return(gsub("[\n,:;.?!\"--–()»«\\/]", " ", txt))
+}
+
+normalizeWhitespace <- function(txt) {
+  
+  # Remove all initial and terminal whitespace.
+  txt <- gsub("^\\s+", "", txt)
+  txt <- gsub("\\s+$", "", txt)
+  
+  # Normalize all whitespace to a blank.
+  txt <- gsub("\\s", " ", txt)
+
+  # Collapse all multiple whitespace to single blank.
+  txt <- gsub("\\s+", " ", txt)
+
+  return(txt)
+}
+
+
+sentenceLength <- function(txt) {
+  # Return a vector of number-of-words in each element of txt
+  sl <- numeric(length(txt))
+  for (i in 1:length(txt)) {
+    sl[i] <- length(strsplit(txt[i], " ")[[1]])
+  }
+  return(sl)
+}
+
+makeWordlist <- function(txt) {
+  # return a vector in which each word is split out on its own from a
+  # sentence-based corpus.
+  
+  return(unlist(strsplit(txt, " ")))
+}
+
+wordLength <- function(wl) {
+  # return a vector of word-lengths for each element of wl.
+  
+  return(nchar(wl))
+}
+
+lemmatize <- function(txt) {
+  # assumes input as one word per element
+  # returns vector with lemmatized form of each word
+  lem <- lemmaDict[txt, 1]
+  nas <- which(is.na(lem))
+  lem[nas] <- txt[nas] # replace words not in dictionary with original
+  return(lem)
+}
+
+getLemmaMisses <- function(txt) {
+  # Utility function. Return a vector of unique tokens of txt that are not
+  # present in the lemma dictionary.
+  return(unique(txt[is.na(lemmaDict[txt, 1])]))
+}
+
+getLemmaTokens <- function(lem) {
+  # Utility function. Return a vector of tokens for the lemma lem.
+  return(rownames(lemmaDict)[lemmaDict$lemma == lem])
+}
 
 # ==== PROCESS =================================================================
 
@@ -539,7 +673,250 @@ printEnrichmentTable(100, AWstemmedWords, WPstemmedWords)
 
 printEnrichmentTable(100, WPstemmedWords, AWstemmedWords)
 
+# ======================================================
+# Next version of analysis: use the xml formatted corpus
+# (August 2016, prepare progress report)
 
+
+AWX <- read_xml("../data/Atemwende/Atemwende.xml")
+AW <- xml_text(xml_find_all(xml_children(AWX)[[1]], ".//s"))
+
+AW <- collapseHyphenation(AW)
+AW <- expandContractions(AW)
+AW <- removePunctuation(AW)
+AW <- normalizeWhitespace(AW)
+AWsl <- sentenceLength(AW) 
+AWcol <- "#CC0000"  # salient red
+
+# get reference corpora from mk2
+MK2X <- read_xml("../data/mk/mk1.i5.xml")
+
+# HF is Max Frisch: Homo Faber (1957)
+HF <- xml_text(xml_find_all(xml_children(MK2X)[[4]], ".//s"))
+HF <- collapseHyphenation(HF)
+HF <- expandContractions(HF)
+HF <- removePunctuation(HF)
+HF <- normalizeWhitespace(HF)
+HFsl <- sentenceLength(HF)
+HFcol <- "#000000"  # rational black
+
+# AC is Heinrich Böll: Ansichten eines Clowns (1963)
+AC <- xml_text(xml_find_all(xml_children(MK2X)[[2]], ".//s"))
+AC <- collapseHyphenation(AC)
+AC <- expandContractions(AC)
+AC <- removePunctuation(AC)
+AC <- normalizeWhitespace(AC)
+ACsl <- sentenceLength(AC)
+ACcol <- "#FF66CC"  # clown pink
+
+# DB is Thomas Mann: Die Betrogene (1953)
+DB <- xml_text(xml_find_all(xml_children(MK2X)[[7]], ".//s"))
+DB <- collapseHyphenation(DB)
+DB <- expandContractions(DB)
+DB <- removePunctuation(DB)
+DB <- normalizeWhitespace(DB)
+DBsl <- sentenceLength(DB)
+DBcol <- "#33FF66"  # greenish
+
+# BZ is Bild Zeitung, ausgewählte Artikel, Juli 1967
+BZ <- xml_text(xml_find_all(xml_children(MK2X)[[29]], ".//s"))
+BZ <- collapseHyphenation(BZ)
+BZ <- expandContractions(BZ)
+BZ <- removePunctuation(BZ)
+BZ <- normalizeWhitespace(BZ)
+BZsl <- sentenceLength(BZ)
+BZcol <- "#884400"  # brownish
+
+# compare sentence length distributions
+
+MAX <- 100  # maximum sentence length considered
+
+
+N <- length(AW)
+MAX <- 100
+nRepeat <- 20
+mHFsl <- numeric(MAX)
+mACsl <- numeric(MAX)
+mDBsl <- numeric(MAX)
+mBZsl <- numeric(MAX)
+
+for (i in 1:nRepeat) {
+  H <- table(sample(HFsl, N))
+  A <- table(sample(ACsl, N))
+  D <- table(sample(DBsl, N))
+  B <- table(sample(BZsl, N))
+  for (j in 1:MAX) {
+    mHFsl[j] <- mHFsl[j] + H[j]
+    mACsl[j] <- mACsl[j] + A[j]
+    mDBsl[j] <- mDBsl[j] + D[j]
+    mBZsl[j] <- mBZsl[j] + B[j]
+  }
+}
+mHFsl <- mHFsl / nRepeat
+mACsl <- mACsl / nRepeat
+mDBsl <- mDBsl / nRepeat
+mBZsl <- mBZsl / nRepeat
+
+
+# Reference corpora
+
+MAXP <- 40
+plot(1:MAXP, mHFsl[1:MAXP], 
+     type = "l",
+     lwd = 1.2,
+     xlim = c(1, MAXP),
+     main = "Sentence length distributions, reference corpora",
+     xlab = "sentence length",
+     ylab = "number of sentences",
+     col = HFcol)
+
+points(1:MAXP, mACsl[1:MAXP], type = "l", lwd = 1.2, col = ACcol)
+points(1:MAXP, mDBsl[1:MAXP], type = "l", lwd = 1.2, col = DBcol)
+points(1:MAXP, mBZsl[1:MAXP], type = "l", lwd = 1.2, col = BZcol)
+
+legend(25,22,
+       c("Frisch (Homo Faber)", 
+         "Böll (Ansichten eines Clowns", 
+         "Mann (Die Betrogene)", 
+         "Bild (Auswahl 1967-07)"),
+       cex = 0.7,
+       lty = 1,
+       lwd = 1.2,
+       bty = "n",
+       col = c( HFcol,  ACcol,  DBcol,  BZcol))
+
+# Atemwende
+
+plot(1:MAXP, as.numeric(table(AWsl))[1:MAXP], 
+     type = "l",
+     lwd = 2.0,
+     xlim = c(1, MAXP),
+     main = "Sentence length distributions, Atemwende comparison",
+     xlab = "sentence length",
+     ylab = "number of sentences",
+     col = AWcol)
+
+
+points(1:MAXP, mHFsl[1:MAXP], type = "l", lwd = 0.8, col = HFcol)
+points(1:MAXP, mACsl[1:MAXP], type = "l", lwd = 0.8, col = ACcol)
+points(1:MAXP, mBZsl[1:MAXP], type = "l", lwd = 0.8, col = BZcol)
+
+legend(25,22,
+       c("Celan (Atemwende)",
+         "Frisch (Homo Faber)",
+         "Böll (Ansichten eines Clowns",
+         "Bild (Auswahl 1967-07)"),
+       cex = 0.7,
+       lty = 1,
+       lwd = c(2.0, 0.8, 0.8, 0.8),
+       bty = "n",
+       col = c(AWcol, HFcol, ACcol, BZcol))
+
+# ==========================================================
+# Compare word length distributions
+
+# Make word vectors
+AWw <- makeWordlist(AW)
+HFw <- makeWordlist(HF)
+ACw <- makeWordlist(AC)
+DBw <- makeWordlist(DB)
+BZw <- makeWordlist(BZ)
+
+# Make word length lists
+AWwl <- wordLength(AWw)
+HFwl <- wordLength(HFw)
+ACwl <- wordLength(ACw)
+DBwl <- wordLength(DBw)
+BZwl <- wordLength(BZw)
+
+MAX <- 25  # maximum word length considered
+N <- length(AWwl)
+nRepeat <- 20
+mHFwl <- numeric(MAX)
+mACwl <- numeric(MAX)
+mDBwl <- numeric(MAX)
+mBZwl <- numeric(MAX)
+
+for (i in 1:nRepeat) {
+  H <- table(sample(HFwl, N))
+  A <- table(sample(ACwl, N))
+  D <- table(sample(DBwl, N))
+  B <- table(sample(BZwl, N))
+  for (j in 1:MAX) {
+    mHFwl[j] <- mHFwl[j] + H[j]
+    mACwl[j] <- mACwl[j] + A[j]
+    mDBwl[j] <- mDBwl[j] + D[j]
+    mBZwl[j] <- mBZwl[j] + B[j]
+  }
+}
+mHFwl <- mHFwl / nRepeat
+mACwl <- mACwl / nRepeat
+mDBwl <- mDBwl / nRepeat
+mBZwl <- mBZwl / nRepeat
+
+
+# Reference corpora
+
+MAXP <- 25
+plot(1:MAXP, mHFwl[1:MAXP], 
+     type = "l",
+     lwd = 1.2,
+     xlim = c(1, MAXP),
+     main = "Word length distributions, reference corpora",
+     xlab = "word length (characters)",
+     ylab = "mean number of words",
+     col = HFcol)
+
+points(1:MAXP, mACwl[1:MAXP], type = "l", lwd = 1.2, col = ACcol)
+points(1:MAXP, mDBwl[1:MAXP], type = "l", lwd = 1.2, col = DBcol)
+points(1:MAXP, mBZwl[1:MAXP], type = "l", lwd = 1.2, col = BZcol)
+
+legend(15,900,
+       c("Frisch (Homo Faber)", 
+         "Böll (Ansichten eines Clowns", 
+         "Mann (Die Betrogene)", 
+         "Bild (Auswahl 1967-07)"),
+       cex = 0.7,
+       lty = 1,
+       lwd = 1.2,
+       bty = "n",
+       col = c( HFcol,  ACcol,  DBcol,  BZcol))
+
+
+# Atemwende
+
+plot(1:MAXP, as.numeric(table(AWwl))[1:MAXP], 
+     type = "l",
+     lwd = 2.0,
+     xlim = c(1, MAXP),
+     main = "Word length distributions, Atemwende comparison",
+     xlab = "word length",
+     ylab = "number of words",
+     col = AWcol)
+
+
+points(1:MAXP, mHFwl[1:MAXP], type = "l", lwd = 0.8, col = HFcol)
+points(1:MAXP, mACwl[1:MAXP], type = "l", lwd = 0.8, col = ACcol)
+points(1:MAXP, mBZwl[1:MAXP], type = "l", lwd = 0.8, col = BZcol)
+
+legend(15, 900,
+       c("Celan (Atemwende)",
+         "Frisch (Homo Faber)",
+         "Böll (Ansichten eines Clowns",
+         "Bild (Auswahl 1967-07)"),
+       cex = 0.7,
+       lty = 1,
+       lwd = c(2.0, 0.8, 0.8, 0.8),
+       bty = "n",
+       col = c(AWcol, HFcol, ACcol, BZcol))
+
+# ==========================================================
+# Compare word frequency distributions
+
+# Lemmatize wordlists
+
+x <- getLemmaMisses(HFw)
+writeLines(x, "lemNA.txt")
 #    
 # ==== TESTS ===================================================================
 
